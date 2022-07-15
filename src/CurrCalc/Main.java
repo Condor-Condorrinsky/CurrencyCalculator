fbe5adc48a41edc3f913f37cea2ab3bcccbecd37
Konrad <konrad.michal.dumin@gmail.com>
2022-07-15 14:42:38 +0200

    poprawka do ścieżki pliku

--- src/CurrCalc/Main.java
+++ src/CurrCalc/Main.java
@@ -2,7 +2,7 @@ package CurrCalc;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "../../resources/prices.xml";
+        String path = "resources/prices.xml";
         Parser parser = new Parser();
         parser.parse(path);
     }
