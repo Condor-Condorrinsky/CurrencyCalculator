6e11fcadaf1842a7aeceb5bf5f2b4b797c642c42
Konrad <konrad.michal.dumin@gmail.com>
2022-07-16 18:59:04 +0200

    parse przyjmuje stringa z zawartością xml, metody pomocnicze do parse i testy jednostkowe

--- src/CurrCalc/Parser.java
+++ src/CurrCalc/Parser.java
@@ -1,6 +1,6 @@
 package CurrCalc;
 
-import javax.xml.XMLConstants;
+//import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -9,27 +9,33 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.math.BigDecimal;
 //import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Parser {
 
     public Parser () {}
 
-    public ArrayList<Currency> parse (String filepath){
+    public ArrayList<Currency> parse (String xmlString){
 
         ArrayList<Currency> currencies = new ArrayList<Currency>();
 
         try {
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document document = db.parse(new File(filepath));
+            // DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            // dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            // DocumentBuilder db = dbf.newDocumentBuilder();
+            //Document document = db.parse(new File(filepath));
+            Document document = loadXMLFromString(xmlString);
 
             document.getDocumentElement().normalize();
             
@@ -61,11 +67,32 @@ public class Parser {
                 }
             }
 
-        } catch (ParserConfigurationException | SAXException | IOException e) {
+        } catch (ParserConfigurationException | IOException | SAXException e) {
             System.out.println("Sorry, something went wrong.");
             e.printStackTrace();
         }
 
         return currencies;
+    }
+
+    private Document loadXMLFromString(String xml)
+    throws ParserConfigurationException, IOException, SAXException, IllegalArgumentException{
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml));
+        return builder.parse(is);
+    }
+
+    public String getResourceFileAsString(String fileName) throws IOException {
+        
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(fileName)) {
+            if (is == null) return null;
+            try (InputStreamReader isr = new InputStreamReader(is);
+                 BufferedReader reader = new BufferedReader(isr)) {
+                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            }
+        }
     }
 }
--- test/CurrCalc/ParserTest.java
+++ test/CurrCalc/ParserTest.java
@@ -1,11 +1,46 @@
 package CurrCalc;
 
-import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
-class ParserTest {
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+
+public class ParserTest {
+
+    static final String FILEPATH = "resources/";
+    private Path workingDir;
+
+    // @Rule
+    // public ResourceRule<String> textResource = givenResource()
+    //         .text("/com/adelean/junit/jupiter/resource.txt")
+    //         .withCharset(StandardCharsets.UTF_8);
+
+    @Before
+    public void init() {
+        this.workingDir = Paths.get("", FILEPATH);
+    }
     
     @Test
-    void test(){
-        
+    public void checkContents(){
+        Parser parser = new Parser();
+        Path input = this.workingDir.resolve("prices.xml");
+        String file = Thread.currentThread().getContextClassLoader().getResource("resources/prices.xml").getFile();
+        String xmlString = null;
+        try {
+            xmlString = parser.getResourceFileAsString(file);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        ArrayList<Currency> list = parser.parse(xmlString);
+        for (Currency curr : list){
+            System.out.println(curr.getName());
+            System.out.println(curr.getPrice());
+        }
     }
 }
