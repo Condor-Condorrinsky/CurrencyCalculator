f4063d7fe7f1a69d2fbad14dac80e147319be465
Konrad <konrad.michal.dumin@gmail.com>
2022-07-16 13:59:32 +0200

    parse zwraca ArrayList z walutami, zalążek testów Parsera

--- src/CurrCalc/Parser.java
+++ src/CurrCalc/Parser.java
@@ -13,13 +13,17 @@ import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.math.BigDecimal;
+//import java.io.InputStream;
+import java.util.ArrayList;
 
 public class Parser {
 
     public Parser () {}
 
-    public void parse (String filepath){
+    public ArrayList<Currency> parse (String filepath){
+
+        ArrayList<Currency> currencies = new ArrayList<Currency>();
 
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -33,6 +37,8 @@ public class Parser {
             //System.out.println("------");
 
             NodeList list = document.getElementsByTagName("Cube");
+
+            currencies.ensureCapacity(list.getLength());
 
             for (int i = 0; i < list.getLength(); i++){
 
@@ -48,8 +54,10 @@ public class Parser {
                     if(currency == null || currency.trim().isEmpty() ||
                     price == null || price.trim().isEmpty()) continue;
 
-                    System.out.println(currency);
-                    System.out.println(price);
+                    //System.out.println(currency);
+                    //System.out.println(price);
+
+                    currencies.add(new Currency(currency, new BigDecimal(price)));
                 }
             }
 
@@ -57,5 +65,7 @@ public class Parser {
             System.out.println("Sorry, something went wrong.");
             e.printStackTrace();
         }
+
+        return currencies;
     }
 }
